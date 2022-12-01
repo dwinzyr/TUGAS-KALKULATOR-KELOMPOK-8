9658e18f22fdf01dcd3ecb663e332f1a9f921d56
Dwi Ananda Surya <118704571+dwinzyr@users.noreply.github.com>
2022-12-01 11:48:19 +0700

    Add files via upload

---
+++ Kalkulator.js
@@ -0,0 +1,11 @@
+const input = require('readline-sync')
+
+console.log('PILIH METODE')
+console.log('Pilih 1,2,3,4')
+var Metode = input.question('1.Tambah|2.Kurang|3.Kali|4.Bagi:')
+
+if(Metode == 1){
+    var Angka1 = input.questionInt('Masukan angka Ke 1:')
+    var Angka2 = input.questionInt('Masukan angka Ke 2:')
+    console.log(Angka1+Angka2)
+}
